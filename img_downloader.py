b0b57a7b6af7e29764fe1886dcc59331236f76fe
Berk Ugudur <berkugudur@hotmail.com>
2019-10-07 20:57:22 +0300

    organizing code, added backup

--- img_downloader.py
+++ img_downloader.py
@@ -1,29 +1,47 @@
 from datetime import datetime
 from itertools import dropwhile, takewhile
+from shutil import copyfile
 
 import instaloader
 import csv
+import os.path
 
-L = instaloader.Instaloader(quiet=True, filename_pattern="{shortcode}", download_videos=False, download_video_thumbnails=False, download_geotags=False,
+def backup_and_create(path):
+    # move last output to backup folder
+    if os.path.exists(path):
+        copyfile(path, "backup/"+ str(datetime.now()) + "_" + path)
+    return open(path, "w+")
+
+def get_usernames(path):
+    usernames = []
+    with open(path) as usernames_file:
+        rows = csv.reader(usernames_file)
+        for row in rows:
+            usernames.append(row[0])
+    return usernames
+
+def download_post(L, username, profile, post, output):
+    row = "{},{},{},{}\n".format(username, profile.followers, post.shortcode, post.likes)
+    print("INFO: {}".format(row), end = '')
+    output.write(row)
+    L.download_post(post, 'photos')
+
+def download_posts(L, usernames, output):
+    SINCE = datetime(2019, 10, 1)
+    UNTIL = datetime(2018, 5, 1)
+    for username in usernames:
+        profile = instaloader.Profile.from_username(L.context, username)
+        posts = profile.get_posts()
+        print( "INFO: Downloading " + username + "'s contents.")
+        for post in takewhile(lambda p: p.date > UNTIL, dropwhile(lambda p: p.date > SINCE, posts)):
+            download_post(L, username, profile, post, output)
+
+def main():
+    L = instaloader.Instaloader(quiet=True, filename_pattern="{shortcode}", download_videos=False, download_video_thumbnails=False, download_geotags=False,
                             download_comments=False, save_metadata=False, compress_json=False, post_metadata_txt_pattern="")
+    output = backup_and_create("output.csv")
+    usernames = get_usernames("usernames.csv")
+    download_posts(L, usernames, output)
 
-SINCE = datetime(2019, 10, 1)
-UNTIL = datetime(2018, 5, 1)
-
-usernames = []
-output = open("output.csv", "w+")
-with open("usernames.csv") as usernames_file:
-    rows = csv.reader(usernames_file)
-    for row in rows:
-        usernames.append(row[0])
-
-for username in usernames:
-    profile = instaloader.Profile.from_username(L.context, username)
-    posts = profile.get_posts()
-    followers = profile.followers
-    print( "INFO: Downloading " + username + "'s contents.")
-    for post in takewhile(lambda p: p.date > UNTIL, dropwhile(lambda p: p.date > SINCE, posts)):
-        row = username + "," + str(followers) + "," + post.shortcode + "," + str(post.likes)
-        print("INFO: " + row)
-        output.write(row + "\n")
-        L.download_post(post, 'photos')
+if __name__ == '__main__':
+    main()
