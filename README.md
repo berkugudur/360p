2a229d4ab353d8014ad3aa474f8438dd6e4be0f4
Berk Ugudur <berkugudur@hotmail.com>
2019-10-07 21:15:53 +0300

    adding static path and readme file

---
+++ README.md
@@ -0,0 +1,3 @@
+Download posts for every username on **usernames.csv**.
+Create **backup** folder inside script location. 
+When you run the script, current **output.csv** will be moved to backup folder with current timestamp in order to keep existing data.
--- img_downloader.py
+++ img_downloader.py
@@ -4,17 +4,31 @@ from shutil import copyfile
 
 import instaloader
 import csv
-import os.path
+import os
 
-def backup_and_create(path):
-    # move last output to backup folder
-    if os.path.exists(path):
-        copyfile(path, "backup/"+ str(datetime.now()) + "_" + path)
-    return open(path, "w+")
+OUTPUT_FILE = "output.csv"
+BACKUP_FOLDER = "backup"
+DOWNLOAD_FOLDER = "photos"
+USERNAMES_FILE = "usernames.csv"
 
-def get_usernames(path):
+def backup_and_create():
+    if not os.path.exists(USERNAMES_FILE):
+        sys.exit("{} file is not exists".format(USERNAMES_FILE))
+
+    if not os.path.exists(BACKUP_FOLDER):
+        os.makedirs(BACKUP_FOLDER)
+
+    if not os.path.exists(DOWNLOAD_FOLDER):
+        os.makedirs(DOWNLOAD_FOLDER)
+
+    if os.path.exists(OUTPUT_FILE):
+        copyfile(OUTPUT_FILE, "{}/{}_{}".format(BACKUP_FOLDER, datetime.now(), OUTPUT_FILE))
+
+    return open(OUTPUT_FILE, "w+")
+
+def get_usernames():
     usernames = []
-    with open(path) as usernames_file:
+    with open(USERNAMES_FILE) as usernames_file:
         rows = csv.reader(usernames_file)
         for row in rows:
             usernames.append(row[0])
@@ -24,7 +38,7 @@ def download_post(L, username, profile, post, output):
     row = "{},{},{},{}\n".format(username, profile.followers, post.shortcode, post.likes)
     print("INFO: {}".format(row), end = '')
     output.write(row)
-    L.download_post(post, 'photos')
+    L.download_post(post, DOWNLOAD_FOLDER)
 
 def download_posts(L, usernames, output):
     SINCE = datetime(2019, 10, 1)
@@ -32,16 +46,15 @@ def download_posts(L, usernames, output):
     for username in usernames:
         profile = instaloader.Profile.from_username(L.context, username)
         posts = profile.get_posts()
-        print( "INFO: Downloading " + username + "'s contents.")
+        print( "INFO: Downloading {}'s contents.".format(username))
         for post in takewhile(lambda p: p.date > UNTIL, dropwhile(lambda p: p.date > SINCE, posts)):
             download_post(L, username, profile, post, output)
 
 def main():
     L = instaloader.Instaloader(quiet=True, filename_pattern="{shortcode}", download_videos=False, download_video_thumbnails=False, download_geotags=False,
                             download_comments=False, save_metadata=False, compress_json=False, post_metadata_txt_pattern="")
-    output = backup_and_create("output.csv")
-    usernames = get_usernames("usernames.csv")
-    download_posts(L, usernames, output)
+    output = backup_and_create()
+    download_posts(L, get_usernames(), output)
 
 if __name__ == '__main__':
     main()
